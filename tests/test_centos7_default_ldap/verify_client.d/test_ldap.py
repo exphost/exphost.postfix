abc770f66422b9bffc6b2223c6356cc846047065
Marcin TORGiren Fabrykowski <torgiren@gmail.com>
2021-06-05 09:47:35 +0200

    add support for ldap auth - closes #348
    Signed-off-by: Marcin TORGiren Fabrykowski <torgiren@gmail.com>
    Change-Id: I874be700b6d47a33529d6931f478388292eee62d
    Signed-off-by: Marcin TORGiren Fabrykowski <torgiren@gmail.com>

--- tests/test_centos7_default_ldap/verify_client.d/test_ldap.py
+++ tests/test_centos7_default_ldap/verify_client.d/test_ldap.py
@@ -6,9 +6,10 @@ def install_connector(host):
         return
     setup_connector(host)
     connector_installed = True
-#def test_postfix_ldap_send_email(host):
-#    install_nc(host)
-#    assert "queued as" in host.check_output("""echo -e "ehlo ja\nAUTH LOGIN\nbWFyaW9sYUBkb21lbmExLnJ1\nbWFyaW9sYXBhc3M=\nMAIL FROM: mariola@domena6.ru\nRCPT TO: basia@client.some.example.domain.xyz\nDATA\nhejo\n.\nquit\n\n" | {connector}""".format(connector=connector))
+
+def test_postfix_ldap_send_email(host):
+    install_connector(host)
+    assert "queued as" in host.check_output("""echo -e "ehlo ja\nAUTH LOGIN\nbWFyaW9sYUBkb21lbmE2LnJ1\nbWFyaW9sYXBhc3M=\nMAIL FROM: mariola@domena6.ru\nRCPT TO: basia@client.some.example.domain.xyz\nDATA\nhejo\n.\nquit\n\n" | {connector}""".format(connector=connector))
 
 def test_postfix_ldap_local_email_delivery(host):
     install_connector(host)
